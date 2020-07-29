8b77b62b8687d0bb4f791ec092a01bbe58a9876d
Andreas Ots <qrpth@qrpth.eu>
2020-07-30 01:47:15 +0300

    renderer: split the frame texture into a texture and a sampler

--- renderer/src/lib.rs
+++ renderer/src/lib.rs
@@ -89,23 +89,48 @@ pub async fn renderer(
         device.create_shader_module(&include!(concat!(env!("OUT_DIR"), "/shader.vert.spv.rs")));
 
     let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-        bindings: &[wgpu::BindGroupLayoutEntry {
-            binding: 0,
-            visibility: wgpu::ShaderStage::FRAGMENT,
-            ty: wgpu::BindingType::SampledTexture {
-                dimension: wgpu::TextureViewDimension::D2,
-                component_type: wgpu::TextureComponentType::Float,
-                multisampled: false,
+        bindings: &[
+            wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStage::FRAGMENT,
+                ty: wgpu::BindingType::SampledTexture {
+                    dimension: wgpu::TextureViewDimension::D2,
+                    component_type: wgpu::TextureComponentType::Float,
+                    multisampled: false,
+                },
             },
-        }],
+            wgpu::BindGroupLayoutEntry {
+                binding: 1,
+                visibility: wgpu::ShaderStage::FRAGMENT,
+                ty: wgpu::BindingType::Sampler { comparison: false },
+            }
+        ],
         label: None,
     });
 
+    let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+        address_mode_u: wgpu::AddressMode::ClampToEdge,
+        address_mode_v: wgpu::AddressMode::ClampToEdge,
+        address_mode_w: wgpu::AddressMode::ClampToEdge,
+        mag_filter: wgpu::FilterMode::Linear,
+        min_filter: wgpu::FilterMode::Linear,
+        mipmap_filter: wgpu::FilterMode::Linear,
+        lod_min_clamp: 0.0,
+        lod_max_clamp: 0.0,
+        compare: wgpu::CompareFunction::Never,
+    });
+
     let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-        bindings: &[wgpu::Binding {
-            binding: 0,
-            resource: wgpu::BindingResource::TextureView(&texture_view),
-        }],
+        bindings: &[
+            wgpu::Binding {
+                binding: 0,
+                resource: wgpu::BindingResource::TextureView(&texture_view),
+            },
+            wgpu::Binding {
+                binding: 1,
+                resource: wgpu::BindingResource::Sampler(&sampler),
+            },
+        ],
         layout: &bind_group_layout,
         label: None,
     });
--- renderer/src/shader.frag.glsl
+++ renderer/src/shader.frag.glsl
@@ -4,7 +4,8 @@ layout(location = 0) in vec2 v_uv;
 
 layout(location = 0) out vec4 color;
 
-layout(binding = 0) uniform sampler2D frame;
+layout(binding = 0) uniform texture2D frame;
+layout(binding = 1) uniform sampler frame_sampler;
 
 void main() {
     mat3 xyz2srgb = mat3(
@@ -12,6 +13,6 @@ void main() {
         -0.96924364,  1.8759675,   0.04155506,
          0.05563008, -0.20397696,  1.05697151
     );
-    vec4 texel = texture(frame, v_uv);
+    vec4 texel = texture(sampler2D(frame, frame_sampler), v_uv);
     color = vec4(texel.xyz * xyz2srgb, texel.w);
 }
